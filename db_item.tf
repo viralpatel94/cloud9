bd641aca5cf00df1a7579e0761dd1a0655d345ee
viralpatel94 <viralpatel94@gmail.com>
2021-10-17 18:09:58 -0500

    changing hash

--- db_item.tf
+++ db_item.tf
@@ -9,25 +9,20 @@ locals {
 
 resource "aws_dynamodb_table_item" "inventory" {
   table_name = "client-environments"
-  hash_key   = var.name
+  hash_key   = "ClientName"
   # range_key  = local.timestamp_sanitized
 
-  #   item = <<ITEM
-  # {
-  #   "ClientName": {"S": "${var.name}"},
-  #   "CreateDate": {"S": "${local.timestamp_sanitized}"},
-  #   "S3": {"S": "${module.cloud9.s3_bucket}"},
-  #   "Cloud9": {"S": "${module.cloud9.cloud9}"},
-  #   "KMS": {"S": "${module.cloud9.kms_id}"},
-  #   "ActiveDays": {"N": "1"},
-  #   "DaysRemaining": {"N": "7"},
-  #   "CreatorName": {"S": "${var.creator}"}
-  # }
-  # ITEM
   item = <<ITEM
-{
-"ClientName": {"S": "test123"},
-"DaysRemaining": {"N": "7"}
-}
-ITEM
+  {
+    "ClientName": {"S": "${var.name}"},
+    "CreateDate": {"S": "${local.timestamp_sanitized}"},
+    "S3": {"S": "${module.cloud9.s3_bucket}"},
+    "Cloud9": {"S": "${module.cloud9.cloud9}"},
+    "KMS": {"S": "${module.cloud9.kms_id}"},
+    "ActiveDays": {"N": "1"},
+    "DaysRemaining": {"N": "7"},
+    "CreatorName": {"S": "${var.creator}"}
+  }
+  ITEM
+
 }
