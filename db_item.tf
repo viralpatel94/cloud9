16c17e5458fd01e5ecf579904b520f7bb8681ed5
viralpatel94 <viralpatel94@gmail.com>
2021-10-17 18:36:44 -0500

    testing1

--- db_item.tf
+++ db_item.tf
@@ -7,21 +7,36 @@ locals {
 
 
 
-resource "aws_dynamodb_table_item" "inventory" {
+# resource "aws_dynamodb_table_item" "inventory" {
+#   table_name = "client-environments"
+#   hash_key   = "ClientName"
+#
+#   item = <<ITEM
+#   {
+#     "ClientName": {"S": "${var.name}"},
+#     "CreateDate": {"S": "${local.timestamp_sanitized}"},
+#     "S3": {"S": "${module.cloud9.s3_bucket}"},
+#     "Cloud9": {"S": "${module.cloud9.cloud9}"},
+#     "KMS": {"S": "${module.cloud9.kms_id}"},
+#     "ActiveDays": {"N": "1"},
+#     "DaysRemaining": {"N": "7"},
+#     "CreatorName": {"S": "${var.creator}"}
+#   }
+#     ITEM
+#
+# }
+
+resource "aws_dynamodb_table_item" "example" {
   table_name = "client-environments"
   hash_key   = "ClientName"
 
   item = <<ITEM
-  {
-    "ClientName": {"S": "${var.name}"},
-    "CreateDate": {"S": "${local.timestamp_sanitized}"},
-    "S3": {"S": "${module.cloud9.s3_bucket}"},
-    "Cloud9": {"S": "${module.cloud9.cloud9}"},
-    "KMS": {"S": "${module.cloud9.kms_id}"},
-    "ActiveDays": {"N": "1"},
-    "DaysRemaining": {"N": "7"},
-    "CreatorName": {"S": "${var.creator}"}
-  }
-  ITEM
-
+{
+  "ClientName": {"S": "something"},
+  "one": {"N": "11111"},
+  "two": {"N": "22222"},
+  "three": {"N": "33333"},
+  "four": {"N": "44444"}
+}
+ITEM
 }
