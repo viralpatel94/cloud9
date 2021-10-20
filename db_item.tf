336e5250756848bdcccdedc82af31a0f7bc9de2a
viralpatel94 <viralpatel94@gmail.com>
2021-10-19 20:47:23 -0500

    removing db

--- db_item.tf
+++ db_item.tf
@@ -28,21 +28,21 @@ locals {
 #
 # }
 
-resource "aws_dynamodb_table_item" "inventory" {
-  table_name = "client-environments"
-  hash_key   = "clientabc"
-
-  item = jsonencode(
-    {
-      "ClientName" : { "S" : "clientabc" },
-      "CreateDate" : { "S" : "20211017224407" },
-      "S3" : { "S" : "arn:aws:s3:::clientabc-codebucket-debugging" },
-      "Cloud9" : { "S" : "arn:aws:cloud9:us-east-1:439380838513:environment:e743ea9743504f11947dfb5192fe61ed" },
-      "KMS" : { "S" : "arn:aws:kms:us-east-1:439380838513:key/55f5f5d5-5329-4d40-9eca-6a687c86c9f0}" },
-      "ActiveDays" : { "N" : "1" },
-      "DaysRemaining" : { "N" : "7" },
-      "CreatorName" : { "S" : "Viral Patel" }
-    }
-  )
-
-}
+# resource "aws_dynamodb_table_item" "inventory" {
+#   table_name = "client-environments"
+#   hash_key   = "clientabc"
+#
+#   item = jsonencode(
+#     {
+#       "ClientName" : { "S" : "clientabc" },
+#       "CreateDate" : { "S" : "20211017224407" },
+#       "S3" : { "S" : "arn:aws:s3:::clientabc-codebucket-debugging" },
+#       "Cloud9" : { "S" : "arn:aws:cloud9:us-east-1:439380838513:environment:e743ea9743504f11947dfb5192fe61ed" },
+#       "KMS" : { "S" : "arn:aws:kms:us-east-1:439380838513:key/55f5f5d5-5329-4d40-9eca-6a687c86c9f0}" },
+#       "ActiveDays" : { "N" : "1" },
+#       "DaysRemaining" : { "N" : "7" },
+#       "CreatorName" : { "S" : "Viral Patel" }
+#     }
+#   )
+#
+# }
