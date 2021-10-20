4d335086713120a9df09e1ccfdc88681726b6a24
viralpatel94 <viralpatel94@gmail.com>
2021-10-20 16:07:34 -0500

    changing cp var

--- cloud9/exec.tf
+++ cloud9/exec.tf
@@ -15,7 +15,7 @@ resource "null_resource" "cp-repo" {
 
   provisioner "local-exec" {
     command = <<EOT
-              git clone https://${data.aws_secretsmanager_secret_version.token_value.secret_string}@github.com/${var.Github_repo_owner}/${var.repo}.git
+              git clone https://${data.aws_secretsmanager_secret_version.token_value.secret_string}@github.com/${var.Github_repo_owner}/${var.Github_repo_name}.git
               AWS_ACCESS_KEY_ID=${var.access_key} AWS_SECRET_ACCESS_KEY=${var.secret_key} aws s3 cp ${var.Github_repo_name} s3://${aws_s3_bucket.b.id}/ --recursive
               rm -rf ${var.Github_repo_name}
     EOT
