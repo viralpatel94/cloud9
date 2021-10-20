2e90471ca7ec40b830d89b98493d73b602eadef2
viralpatel94 <viralpatel94@gmail.com>
2021-10-19 20:53:12 -0500

    changing to right resource

--- db_item.tf
+++ db_item.tf
@@ -9,24 +9,24 @@ locals {
 
 
 
-# resource "aws_dynamodb_table_item" "inventory" {
-#   table_name = "client-environments"
-#   hash_key   = "clientabc"
-#
-#   item = jsonencode(
-#     {
-#       "ClientName" : { "S" : "${var.name}" },
-#       "CreateDate" : { "S" : "${local.timestamp_sanitized}" },
-#       "S3" : { "S" : "${module.cloud9.s3_bucket}" },
-#       "Cloud9" : { "S" : "${module.cloud9.cloud9}" },
-#       "KMS" : { "S" : "${module.cloud9.kms_id}" },
-#       "ActiveDays" : { "N" : "1" },
-#       "DaysRemaining" : { "N" : "7" },
-#       "CreatorName" : { "S" : "${var.creator}" }
-#     }
-#   )
-#
-# }
+resource "aws_dynamodb_table_item" "inventory" {
+  table_name = "client-environments"
+  hash_key   = "ClientName"
+
+  item = jsonencode(
+    {
+      "ClientName" : { "S" : "${var.name}" },
+      "CreateDate" : { "S" : "${local.timestamp_sanitized}" },
+      "S3" : { "S" : "${module.cloud9.s3_bucket}" },
+      "Cloud9" : { "S" : "${module.cloud9.cloud9}" },
+      "KMS" : { "S" : "${module.cloud9.kms_id}" },
+      "ActiveDays" : { "N" : "1" },
+      "DaysRemaining" : { "N" : "7" },
+      "CreatorName" : { "S" : "${var.creator}" }
+    }
+  )
+
+}
 
 # resource "aws_dynamodb_table_item" "inventory" {
 #   table_name = "client-environments"
